79758544062d0eb7203a273fbe35029472bf16e1
Linkraft <jordan.smiths.games@gmail.com>
2021-01-16 18:10:31 -0500

    Initial commit

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public string roomCode = "Room 1";
+    public int maxPlayers = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConnectToServer();
+    }
+
+    void ConnectToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Try Connect To Server...");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected To Server.");
+        base.OnConnectedToMaster();
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = (byte)maxPlayers;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+        PhotonNetwork.JoinOrCreateRoom(roomCode, roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a Room.");
+        base.OnJoinedRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("A new player joined the room.");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+}
