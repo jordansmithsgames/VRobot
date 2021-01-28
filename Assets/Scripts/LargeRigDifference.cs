7ca9a4ba14fdc72c8aeb46c0f2e7cf20be494887
patrickmolen <35581599+patrickmolen@users.noreply.github.com>
2021-01-28 10:45:44 -0500

    Lerps and Slerps

--- Assets/Scripts/LargeRigDifference.cs
+++ Assets/Scripts/LargeRigDifference.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LargeRigDifference : MonoBehaviour
-{
+{ 
+    //Robot rig IK targets
     public GameObject largeTargetL;
     public GameObject largeTargetR;
 
+    //User rig IK targets
     public GameObject smallTargetL;
     public GameObject smallTargetR;
 
+    //Robot movement speed
+    public int speed = 4;
+
     void Update()
     {
-        largeTargetL.transform.localPosition = smallTargetL.transform.localPosition;
-        largeTargetR.transform.localPosition = smallTargetR.transform.localPosition;
-        
-        largeTargetL.transform.localRotation = smallTargetL.transform.localRotation;
-        largeTargetR.transform.localRotation = smallTargetR.transform.localRotation;
+        Vector3 posOneL = smallTargetL.transform.localPosition;
+        Vector3 posOneR = smallTargetR.transform.localPosition;
+
+        largeTargetL.transform.localPosition = Vector3.Lerp(largeTargetL.transform.localPosition, posOneL, Time.deltaTime * speed);
+        largeTargetR.transform.localPosition = Vector3.Lerp(largeTargetR.transform.localPosition, posOneR, Time.deltaTime * speed);
+
+        Quaternion rotOneL = smallTargetL.transform.localRotation;
+        Quaternion rotOneR = smallTargetR.transform.localRotation;
+
+        largeTargetL.transform.localRotation = Quaternion.Slerp(largeTargetL.transform.localRotation, rotOneL, Time.deltaTime * speed);
+        largeTargetR.transform.localRotation = Quaternion.Slerp(largeTargetR.transform.localRotation, rotOneR, Time.deltaTime * speed);
     }
 }
