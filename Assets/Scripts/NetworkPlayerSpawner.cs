e65dfecb91ad8745d5dd88fe17a511164add59cf
Smith <linkraft@ufl.edu>
2021-02-09 12:22:20 -0500

    Build

--- Assets/Scripts/NetworkPlayerSpawner.cs
+++ Assets/Scripts/NetworkPlayerSpawner.cs
@@ -5,12 +5,25 @@ using Photon.Pun;
 
 public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
 {
+    public Transform spawnpoint1, spawnpoint2;
     private GameObject spawnedPlayerPrefab;
 
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
-        spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", transform.position, transform.rotation);
+        // Player 1
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Player 1 joined!");
+            spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", spawnpoint1.position, spawnpoint1.rotation);
+        }
+
+        // Player 2
+        else
+        {
+            Debug.Log("Player 2 joined!");
+            spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", spawnpoint2.position, spawnpoint2.rotation);
+        }
     }
 
     public override void OnLeftRoom()
