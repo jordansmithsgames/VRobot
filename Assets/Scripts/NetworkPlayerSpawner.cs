08d8673d5efa538597f342ec32df810c97148e6d
Smith <linkraft@ufl.edu>
2021-02-11 12:28:16 -0500

    Some work done for robot rig

--- Assets/Scripts/NetworkPlayerSpawner.cs
+++ Assets/Scripts/NetworkPlayerSpawner.cs
@@ -6,6 +6,7 @@ using Photon.Pun;
 public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
 {
     public Transform spawnpoint1, spawnpoint2;
+    public GameObject xrRig;
     private GameObject spawnedPlayerPrefab;
 
     public override void OnJoinedRoom()
@@ -16,6 +17,8 @@ public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
         {
             Debug.Log("Player 1 joined!");
             spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", spawnpoint1.position, spawnpoint1.rotation);
+            xrRig.transform.position = spawnedPlayerPrefab.transform.position;
+            xrRig.transform.rotation = spawnedPlayerPrefab.transform.rotation;
         }
 
         // Player 2
@@ -23,6 +26,8 @@ public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
         {
             Debug.Log("Player 2 joined!");
             spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", spawnpoint2.position, spawnpoint2.rotation);
+            xrRig.transform.position = spawnedPlayerPrefab.transform.position;
+            xrRig.transform.rotation = spawnedPlayerPrefab.transform.rotation;
         }
     }
 
