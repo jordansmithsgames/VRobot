fc1c9f7b281d994c3590391da5bb8df826bad353
patrickmolen <35581599+patrickmolen@users.noreply.github.com>
2021-02-24 17:30:02 -0500

    Destructable test

---
+++ Assets/Destructable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hit");
+        GameObject piece;
+        piece = collision.gameObject;
+        piece.GetComponent<Rigidbody>().useGravity = true;
+        piece.GetComponent<Rigidbody>().isKinematic = false;
+        
+    }
+}
