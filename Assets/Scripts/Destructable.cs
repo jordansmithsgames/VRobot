f3046cde7b0c924d50ac0eef75c80e9ed4256516
patrickmolen <35581599+patrickmolen@users.noreply.github.com>
2021-02-26 14:11:33 -0500

    added particles and scripts

---
+++ Assets/Scripts/Destructable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    public GameObject particles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particles.AddComponent<DestructionRemove>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Destructable"))
+        {
+            Debug.Log("Hit");
+            GameObject piece;
+            piece = collision.gameObject;
+            
+            //Check if object has already been hit
+            if (piece.GetComponent<Rigidbody>().isKinematic == true)
+            {
+                Instantiate(particles, transform.position, transform.rotation);
+            }
+
+            // Make objects move
+            piece.GetComponent<Rigidbody>().useGravity = true;
+            piece.GetComponent<Rigidbody>().isKinematic = false;
+            piece.AddComponent<DestructionRemove>();
+        }
+    }
+}
---
+++ Assets/Scripts/DestructionRemove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructionRemove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float delay = 5f;
+
+    float countdown;
+    public bool hit;
+    bool removed = false;
+    void Start()
+    {
+        countdown = delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            countdown -= Time.deltaTime;
+            if (countdown <= 0f && removed == false)
+            {
+                Destroy(this.gameObject);
+                removed = true;
+            }
+        
+    }
+}
