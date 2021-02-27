907003af9d5ec291d4b0b0b9a38d5f110f2a5fe1
patrickmolen <35581599+patrickmolen@users.noreply.github.com>
2021-02-27 09:39:07 -0500

    Particle performance script

--- Assets/Scripts/Destructable.cs
+++ Assets/Scripts/Destructable.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Destructable : MonoBehaviour
 {
     public GameObject particlesLarge;
+
+    public int pieceCountMax = 15;
+
+    Queue pieceCount = new Queue();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +19,8 @@ public class Destructable : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   
+        Debug.Log("Piece Queue count is: " + pieceCount.Count);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -24,17 +30,32 @@ public class Destructable : MonoBehaviour
             Debug.Log("Hit");
             GameObject piece;
             piece = collision.gameObject;
-            
+         
             //Check if object has already been hit
             if (piece.GetComponent<Rigidbody>().isKinematic == true)
             {
-                Instantiate(particlesLarge, transform.position, transform.rotation);
+                Instantiate(particlesLarge, piece.transform.position, piece.transform.rotation);
             }
+
+            queueHandler(piece);
 
             // Make objects move
             piece.GetComponent<Rigidbody>().useGravity = true;
             piece.GetComponent<Rigidbody>().isKinematic = false;
-            piece.AddComponent<DestructionRemove>();
+            //piece.AddComponent<DestructionRemove>();
+        }
+    }
+
+    public void queueHandler(GameObject destructablePiece)
+    {
+        GameObject pieceObject = destructablePiece;
+
+        pieceCount.Enqueue(pieceObject);
+
+        //makes sure theres never more than 15 destruction particle effects in the scene
+        if (pieceCount.Count > pieceCountMax)
+        {
+            Destroy((GameObject)pieceCount.Dequeue());
         }
     }
 }
