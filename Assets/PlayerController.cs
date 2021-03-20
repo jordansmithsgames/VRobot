9ad2bd6f52793369c202031da32c121dac07c598
Smith <linkraft@ufl.edu>
2021-03-20 19:36:14 -0400

    Controller mostly there

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("IgnoreCollision"))
+            Physics.IgnoreCollision(this.GetComponent<Collider>(), collision.collider);
+        else Debug.Log(collision.gameObject.name);
+    }
+}
