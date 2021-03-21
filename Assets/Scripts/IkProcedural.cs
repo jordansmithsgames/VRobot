18578e4e07f80b9b3e2c6a1f9151f9fad07b2c6b
patrickmolen <35581599+patrickmolen@users.noreply.github.com>
2021-03-20 21:32:14 -0400

    Proced IK Walk

--- Assets/Scripts/IkProcedural.cs
+++ Assets/Scripts/IkProcedural.cs
@@ -23,7 +23,7 @@ public class IkProcedural : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        footSpace = transform.localPosition.x;
+        footSpace = transform.localPosition.z;
         currentPosition = newPosition = oldPosition = transform.position;
         currentNormal = newNormal = oldNormal = transform.up;
         lerp = 1;
@@ -36,14 +36,14 @@ public class IkProcedural : MonoBehaviour
         transform.up = currentNormal;
 
 
-        Ray ray = new Ray(body.transform.position + (body.transform.right * footSpace), Vector3.down);
+        Ray ray = new Ray(body.transform.position + (body.transform.forward * footSpace), Vector3.down);
         if (Physics.Raycast(ray, out RaycastHit info, 10, terrainLayer.value))
         {
             if (Vector3.Distance(newPosition, info.point) > stepDistance && !otherFoot.IsMoving() && lerp >= 1)
             {
                 lerp = 0;
-                int direction = body.transform.InverseTransformPoint(info.point).z > body.transform.InverseTransformPoint(newPosition).z ? 1 : -1;
-                newPosition = info.point + (body.transform.forward * stepLength * direction) + footOffset;
+                int direction = body.transform.InverseTransformPoint(info.point).x > body.transform.InverseTransformPoint(newPosition).x ? 1 : -1;
+                newPosition = info.point + (body.transform.right * stepLength * direction) + footOffset;
                 newNormal = info.normal;
             }           
         }
---
+++ Assets/Scripts/RobotWalker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RobotWalker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    NavMeshAgent agent;
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(new Vector3(Random.Range(25f, 100f), 0, Random.Range(25f, 100f)));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
