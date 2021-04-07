a695a3e8ba32f1b7c7a85351d72902e166b5635d
Gowthaman <aadithyagow@gmail.com>
2021-04-07 16:48:06 -0400

    effect tjsdgs

---
+++ Assets/RotateScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+    public Transform target;
+    public float movementTime = 1;
+    public float rotationSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, rotationSpeed * Time.deltaTime);
+    }
+}
